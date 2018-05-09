484ea07d7f0bfc2a51d879808c1a1a6ae1a9d4ab
Mateusz Then <Mateusz@MacBook-Air-Mateusz.local>
2018-05-09 09:41:54 +0200

    zmiana

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,23 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+                String result = simpleUser.getUsername();
+
+        if(result.equals("theForumUser")){
+            System.out.println(("test OK"));
+        }else{
+            System.out.println("Error");
+        }
     }
+
+    Calculator calculator = new Calculator (15, 10);
+        int addResult = calculator.add();
+        int substractResult = calculator.substract();
+
+        if(addResult > )
 }
--- kodilla-testing/src/main/java/com/kodilla/testing/user/SimpleUser.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/user/SimpleUser.java
@@ -1,4 +1,12 @@
 package com.kodilla.testing.user;
 
 public class SimpleUser {
+    private String username;
+    public SimpleUser(String username){
+        this.username = username;
+    }
+
+    public String getUsername(){
+        return this.username;
+    }
 }
