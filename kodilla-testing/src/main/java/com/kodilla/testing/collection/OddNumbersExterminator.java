300df8276315cc3f877cb7a55299f55300c8a227
Mateusz Then <Mateusz@MacBook-Air-Mateusz.local>
2018-05-09 17:09:09 +0200

    zadanie 6.3 cd

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -4,6 +4,13 @@ import java.util.ArrayList;
 
 public class OddNumbersExterminator {
     public void exterminate(ArrayList<Integer> numbers){
-
+        int temporaryValue = 0;
+        ArrayList<Integer> theList = new ArrayList<>();
+        for(int n = 0; n < numbers.size(); n++){
+            temporaryValue = numbers.get(n);
+            if(temporaryValue % 2 == 0){
+                theList.add(temporaryValue);
+            }
+        }
     }
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -1,4 +1,38 @@
 package com.kodilla.testing.collection;
 
+import com.kodilla.testing.collection.OddNumbersExterminator;
+import org.junit.*;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+
 public class CollectionTestSuite {
+    @Before
+    public void before(){
+        System.out.println("Test case: Begin");
+    }
+    @After
+    public void after(){
+        System.out.println("Test case: End");
+    }
+    @BeforeClass
+    public static void beforeClass(){
+        System.out.println("Test suite: Begin");
+    }
+    @AfterClass
+    public static void afterClass(){
+        System.out.println("Test suite: End");
+    }
+    @Test
+    public void testOddNumbersExterminatorEmptyList(){
+        //Given
+        ArrayList<Integer> testList = new ArrayList<>();
+        Random theGenerator = new Random();
+        for(int n = 0; n < 20; n++){
+            testList.add(theGenerator.nextInt(50));
+        //When
+
+        }
+    }
 }
