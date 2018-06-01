ecc969e8380d3e7ccfbdd844fc6b05608206abc3
Mateusz Then <Mateusz@MacBook-Air-Mateusz.local>
2018-06-01 16:01:13 +0200

    zadanie 7.4 poprawki

--- kodilla-stream/src/test/java/com/kodilla/stream/world/WorldTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/world/WorldTestSuite.java
@@ -1,17 +1,31 @@
 package com.kodilla.stream.world;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.math.BigDecimal;
 
 public class WorldTestSuite {
     @Test
     public void testGetPeopleQuantity(){
         //Given
-        World testWorld = new World();
+        Country country1 = new Country("Poland", new BigDecimal(38000000));
+        Country country2 = new Country("Germany", new BigDecimal(6000000));
+        Country country3 = new Country("Spain", new BigDecimal(7674848));
+
+        Continent continent1 = new Continent("Europe");
+
+        World earth = new World();
 
         //When
+        continent1.addCountry(country1);
+        continent1.addCountry(country2);
+        continent1.addCountry(country3);
 
+        earth.addContinent(continent1);
 
         //Then
+        Assert.assertEquals(51674848, earth.getPeopleQuantity());
 
     }
 }
