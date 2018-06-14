aec40f10339aa74aa936be351bd2eb17554b4b02
Mateusz Then <Mateusz@MacBook-Air-Mateusz.local>
2018-06-14 16:03:54 +0200

    9.2

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/ProductOrderService.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/ProductOrderService.java
@@ -1,5 +1,16 @@
 package com.kodilla.good.patterns.challenges;
 
 public class ProductOrderService {
+    private InformationService informationService;
+    private SellService sellService;
+    private SendOperations sendOperations;
 
+    public ProductOrderService(final InformationService informationService, final SellService sellService,
+                               final SendOperations sendOperations) {
+        this.informationService = informationService;
+        this.sellService = sellService;
+        this.sendOperations = sendOperations;
+    }
+
+    public SellDto process()
 }
