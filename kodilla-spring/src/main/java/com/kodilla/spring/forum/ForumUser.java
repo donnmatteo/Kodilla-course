bbbc6efc5b9cb17979b705f042a8b0594e64ec01
Mateusz Then <Mateusz@MacBook-Air-Mateusz.local>
2018-06-21 21:22:26 +0200

    10.1 corrected

--- kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
@@ -1,13 +1,18 @@
 package com.kodilla.spring.forum;
 
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ForumUser {
-    String username;
+    private String username;
 
     public ForumUser() {
         username = "John Smith";
+    }
+
+    public String getUsername() {
+        return username;
     }
 }
