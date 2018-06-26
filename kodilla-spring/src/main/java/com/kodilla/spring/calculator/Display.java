d21d945aa68ae40e16181a6d805e425f103b19ed
Mateusz Then <Mateusz@MacBook-Air-Mateusz.local>
2018-06-26 08:17:23 +0200

    8.2 finished

--- kodilla-spring/src/main/java/com/kodilla/spring/calculator/Display.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Display.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class Display {
     public void displayValue (double val) {
-
+        System.out.println("Result: " + val);
     }
 }
